56fee17856da1f79bb58c6a0566b30577b907203
PolkovnikovaA <21nastya.chernaya19@gmail.com>
2023-03-19 23:28:55 +0300

    fix Zapis

--- Zadanie_1_UP/Page/Zapis.xaml.cs
+++ Zadanie_1_UP/Page/Zapis.xaml.cs
@@ -83,43 +83,28 @@ namespace Zadanie_1_UP
 
         public void Details_Service_Admin()
         {
-            /*Image_Delete.Visibility = Visibility.Visible;
-            Image_Izm.Visibility = Visibility.Visible;
-            Text_service1.Visibility = Visibility.Visible;
-            Text_pric.Visibility = Visibility.Visible;
-            Text_lab.Visibility = Visibility.Visible;
-            Text_service12.Visibility = Visibility.Hidden;
-            Text_pric2.Visibility = Visibility.Hidden;
-            Text_lab2.Visibility = Visibility.Hidden;
             Zapiss.Visibility = Visibility.Hidden;
+            Sotr2.Visibility = Visibility.Hidden;
+            Service2.Visibility = Visibility.Hidden;
+            Price2.Visibility = Visibility.Hidden;
+
+            Sotr1.Visibility = Visibility.Visible;
+            Service.Visibility = Visibility.Visible;
+            Price.Visibility = Visibility.Visible;
+            Delete.Visibility = Visibility.Visible;
+            Izm.Visibility = Visibility.Visible;
 
             services = Entities.GetContext().Service.ToList();
             for (int i = 0; i < services.Count; i++)
             {
                 if (services[i].service1 == Item.GetType().GetProperty("service1").GetValue(Item))
                 {
-                    Text_service1.Text = services[i].service1;
-                    Text_pric.Text = services[i].price.ToString();
-                    Text_lab.Text = services[i].Sotr.ToString();
-                    if (services[i].Sotr == "Мулянов Андрей Александрович")
-                    {
-                        var uriImageSource = new Uri(@"/Zadanie_1_UP;component/Image/Sotr1.png", UriKind.RelativeOrAbsolute);
-                        Image.Source = new BitmapImage(uriImageSource);
-                    }
-                    else if (services[i].Sotr == "Алешкина Варвара Владимировна")
-                    {
-                        var uriImageSource = new Uri(@"/Zadanie_1_UP;component/Image/Sotr2.png", UriKind.RelativeOrAbsolute);
-                        Image.Source = new BitmapImage(uriImageSource);
-                    }
-                    else
-                    {
-                        var uriImageSource = new Uri(@"/Zadanie_1_UP;component/Image/zagl.jpg", UriKind.RelativeOrAbsolute);
-                        Image.Source = new BitmapImage(uriImageSource);
-                    }
+                    Sotr1.Text = services[i].Workers.name;
+                    Service.Text = services[i].service1;
+                    Price.Text = Convert.ToString(services[i].price);
                     break;
                 }
-            }*/
-
+            }
         }
 
         public void Details_Service_User()
@@ -139,15 +124,15 @@ namespace Zadanie_1_UP
 
         private void Zapis_Delete(object sender, RoutedEventArgs e)
         {
-            List<Service> ser = new List<Service> { };
-            ser = Entities.GetContext().Service.ToList();
+            List<Service> services = new List<Service> { };
+            services = Entities.GetContext().Service.ToList();
             try
             {
-                for (int i = 0; i < ser.Count; i++)
+                for (int i = 0; i < services.Count; i++)
                 {
-                    if (ser[i].service1 == Item.GetType().GetProperty("service1").GetValue(Item))
+                    if (services[i].service1 == Item.GetType().GetProperty("service1").GetValue(Item))
                     {
-                        Entities.GetContext().Service.Remove(ser[i]);
+                        Entities.GetContext().Service.Remove(services[i]);
                         Entities.GetContext().SaveChanges();
                         break;
                     }
@@ -160,75 +145,22 @@ namespace Zadanie_1_UP
             }
         }
 
-        //Штрих-код
-       /* public void Generate_Code()
-        {
-            var imageType = "Png";
-
-            var imageFormat = (BarCodeImageFormat)Enum.Parse(typeof(BarCodeImageFormat), imageType.ToString());
-            var encodeType = EncodeTypes.Code128;
-            BarcodeGen barcode = new BarcodeGen();
-
-            String allowchar = " ";
-            allowchar += "1,2,3,4,5,6,7,8,9,0";
-            char[] a = { ',' };
-            String[] ar = allowchar.Split(a);
-            String pwd = "";
-            string temp = "";
-            Random r = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                temp = ar[(r.Next(0, ar.Length))];
-                pwd += temp;
-            }
-            barcode.Text = pwd;
-
-            barcode.BarcodeType = encodeType;
-            barcode.ImageType = imageFormat;
-
-            try
-            {
-                string imagePath = GenerateBarcode(barcode);
-
-
-                Uri fileUri = new Uri(Path.GetFullPath(imagePath));
-               //imgDynamic.Source = new BitmapImage(fileUri);
-            }
-            catch
-            {
-                MessageBox.Show("Ошибка");
-            }
-        }
-
-        private string GenerateBarcode(BarcodeGen barcode)
-        {
-            // Image path
-            string imagePath = "." + barcode.ImageType;
-
-            // Initilize barcode generator
-            BarcodeGenerator generator = new BarcodeGenerator(barcode.BarcodeType, barcode.Text);
-
-            // Save the image
-            generator.Save(imagePath, barcode.ImageType);
-
-            return imagePath;
-        }*/
-
         private void Zapis_Izm(object sender, RoutedEventArgs e)
         {
-            List<Service> ser = new List<Service>();
-            ser = Entities.GetContext().Service.ToList();
+            List<Service> services = new List<Service>();
+            services = Entities.GetContext().Service.ToList();
             try
             {
-                for (int i = 0; i < ser.Count; i++)
+                for (int i = 0; i < services.Count; i++)
                 {
-                    if (ser[i].service1 == Item.GetType().GetProperty("service1").GetValue(Item))
+                    if (services[i].service1 == Item.GetType().GetProperty("service1").GetValue(Item))
                     {
-                        ser[i].service1 = Text_service1.Text;
-                        ser[i].price = Convert.ToInt32(Text_pric.Text);
-                        
-                        if (ser[i].image == null)
+                        services[i].Workers.name = Sotr1.Text;
+                        services[i].service1 = Service.Text;
+                        services[i].price = Convert.ToDouble(Price.Text);
+                        if (services[i].image == null)
                         {
+                            //рандом
                             String allowchar = " ";
                             allowchar += "1,2,3,4,5,6,7,8,9,0";
                             char[] a = { ',' };
@@ -242,26 +174,14 @@ namespace Zadanie_1_UP
                                 pwd += temp;
                             }
 
+                            //генератор штрихкода
                             BarcodeGenerator generator = new BarcodeGenerator(EncodeTypes.Code128, pwd);
                             var imageType = "Png";
-
                             string imagePath = "barcode" + pwd + ".Png";
-
                             string path = System.IO.Path.GetFullPath(imagePath);
-
                             generator.Save(imagePath, BarCodeImageFormat.Png);
-                            ser[i].image = path;
-
-
+                            services[i].image = path;
                         }
-                        else
-                        {
-                            if (img.FileName != "")
-                            {
-                                ser[i].image = img.FileName;
-                            }
-                        }
-                      //  ser[i].Sotr = Text_lab.Text;
                         break;
                     }
                 }
